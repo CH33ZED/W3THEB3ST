45efe34493e67654ed205d52e8ed4b13a4dc7f40
CH33ZED <zlin3@stuy.edu>
2016-01-15 09:53:42 -0500

    Updated

--- Tank.java
+++ Tank.java
@@ -1,5 +1,37 @@
 public class tank extends Ship{
     public String toString(){
-        return "=<" + HP + "}]"
+        return "[}" + HP + "}]" + gun;
     }
+    public void setMove(String a){
+        if(a = "w"){
+            if(r > 0){
+                r--;
+            }
+	else{
+            if(r < 9){
+                r++;
+            }
+        }
+    }
+    public void fire(){
+        if (gun.equals(")="){
+	      bullet a = bullet(r,c+1);
+	}
+	else{
+	    if(ammo % 3 == 0){
+	      bullet a = bullet(r,c+1);
+            }
+        }     	  
+    }
+    public upgrade(int a){
+        setHP(HP + a);
+    }
+    public setGun(String a){
+        gun = a;
+    }
+    public setHP(int a){
+        HP = a;
+    }  
+    public special(){
+    }                                    
 }
