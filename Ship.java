3512eb13a2a5279777f4c8090f4145339a75456f
CH33ZED <zlin3@stuy.edu>
2016-01-19 14:51:38 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -2,6 +2,14 @@ public abstract class Ship{
     public String name;
     public String gun; 
     public int HP;
+    r = 4;
+           c = o;
+           HP = 2;
+           gun = "-";
+           name = "Agility";
+           nuke = false;
+           hit = false;
+           kc ;
     public abstract void move(String a);
     public abstract void fire();
     public abstract void upgrade(int a);  
