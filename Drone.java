748787be6d938120271c4d9351331de9c8723000
CH33ZED <zlin3@stuy.edu>
2016-01-20 09:54:13 -0500

    powerup and drones added

---
+++ Drone.java
@@ -0,0 +1,39 @@
+public class Drone extends Ship{
+    public Drone(){
+	   r = (int)(Math.random()*10);
+           c = 20;
+           HP = 1;
+           gun = "-";
+           name = "Drone";
+           nuke = false;
+           hit = false;
+           kc = 0;
+           dead = false;
+    }
+    public String toString(){
+        return "<}";
+    }
+    public void setMove(String a){
+        if(c > 0){
+	   c--;
+        }
+        else{
+	   this.die();
+	}
+    }
+    public void fire(){
+    }
+    
+    public void upgrade(int a){
+    }
+    public void setGun(String a){
+    }
+    public void setHP(int a){
+    }  
+    public void special(){
+    }
+    public void die(){
+         name = "X";
+         dead = true;
+    }                         
+}
