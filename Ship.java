0c4f1c39a861784b6941fb960382822c45e65de5
CH33ZED <zlin3@stuy.edu>
2016-01-25 00:12:51 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,19 +1,19 @@
-public abstract class Ship{
-    public String name;
-    public String gun; 
-    public int r;
-    public int c;
-    public int HP;
-    public boolean nuke;
-    public boolean hit;
-    public int kc;
-    public int ammo;
-    public boolean dead;
-    public abstract void setMove(String a);
-    public abstract int fire();
-    public abstract void upgrade(int a);  
-    public abstract void setGun(String a);
-    public abstract void setHP(int a);
-    public abstract void special();
-    public abstract void die(); 
+public abstract class Ship{                                          //Super class of every object in this game
+    public String name;                                              //used for identification purposes          
+    public String gun;                                               //used to spawn the corresponding type of bullets during fire()                      
+    public int r;                                                    //row index                               
+    public int c;                                                    //column index                   
+    public int HP;                                                   //health                     
+    public boolean nuke;                                             //nuke equipped for fire()>                        
+    public boolean hit;                                              //if the ship is hit
+    public int kc;                                                   //number of kills the ship has                     
+    public int ammo;                                                 //weapon reload progress        
+    public boolean dead;                                             //is the unit dead? (not used)                           
+    public abstract void setMove(String a);                          //set new position to move to         
+    public abstract int fire();                                      //return an int indicating bullet type                     
+    public abstract void upgrade(int a);                             //upgrades ship(not used)      
+    public abstract void setGun(String a);                           //set gun as String a               
+    public abstract void setHP(int a);                               //add int a to HP          
+    public abstract void special();                                  //ship's special ability                 
+    public abstract void die();                                      //die
 }
